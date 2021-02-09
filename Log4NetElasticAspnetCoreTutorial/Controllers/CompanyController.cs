ac33676391e3a00ea1317b8e43b9ac62ad2c45e2
Furkan Katman <furkan.katman@gmail.com>
2021-02-09 18:16:47 +0300

    #Elastic'e başarılı şekilde loglar yazıldı.

---
+++ Log4NetElasticAspnetCoreTutorial/Controllers/CompanyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Log4NetElasticAspnetCoreTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ILogger<CompanyController> _logger;
+
+        public CompanyController(ILogger<CompanyController> logger)
+        {
+            
+            this._logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Company(int id)
+        {
+            _logger.LogInformation(string.Format("#####Company Controller's httpget method is called#####"));
+            var companys =new string[]{"DEFACTO","DEFACTO TEKNOLOJİ","Koç","Arçelik","Beko","Vestel","Empolka Metal" };
+            _logger.LogInformation(string.Format("#####Company Controller's httpget method returned {0}#####", JsonConvert.SerializeObject(companys)));
+            return Ok(JsonConvert.SerializeObject(companys));
+        }
+    }
+}
